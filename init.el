2624b0e0f643669b57f34d060630155d14f7d89e
vemv <vemv@users.noreply.github.com>
2017-11-04 20:04:48 +0100

    Disable minibuffer annoyances

--- init.el
+++ init.el
@@ -8,6 +8,8 @@
   (setq inhibit-message t) ;; Silence minibuffer
   (setq debug-on-error nil)
   (setq debugger (lambda (&rest _))) ;; Disable annoying *Backtrace* buffer
+  (setq command-error-function (lambda (&rest _))) ;; Silence "End of buffer" messages
+  (defun minibuffer-message (&rest _)) ;; Silence "No matching parenthesis found"
   
   (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
   (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
