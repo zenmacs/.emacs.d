a12052b36566a1c0eb66a880e1223c1e1e67570d
vemv <vemv@users.noreply.github.com>
2018-01-13 07:35:41 +0100

    Don't search non-project files in project-explorer

--- lib/non-submodules/vemv.lang.el
+++ lib/non-submodules/vemv.lang.el
@@ -422,40 +422,44 @@ Unconditionally removing code may yield semantically wrong results, i.e. leaving
   (let ((default-directory (replace-regexp-in-string "\\.$" "" (ido-read-file-name ()))))
     (call-interactively 'project-explorer-open)))
 
-(defun vemv/show-current-file-in-project-explorer ()
+(defun vemv/show-current-file-in-project-explorer-unsafe ()
   (interactive)
-  (if (minibuffer-prompt)
-    (delay 'vemv/show-current-file-in-project-explorer 1)
-
-    (vemv/refresh-file-caches)
-    (vemv/safe-select-window vemv/main_window)
+  (let ((fallback (argless (funcall vemv/safe-show-current-file-in-project-explorer))))
     (if (minibuffer-prompt)
-      (delay 'vemv/show-current-file-in-project-explorer 1)
-      (let* ((buffer-fragments (-remove (lambda (x) (string-equal x "")) (split-string (file-truename (buffer-file-name)) "/")))
-             (projname (pe/project-root-function-default)) ;; "/Users/vemv/gpm"
-             (project-fragments (-remove (lambda (x) (string-equal x "")) (split-string projname "/")))
-             (fragments (-drop (length project-fragments) buffer-fragments))
-             (expanded-fragments (mapcar* (lambda (x y) (-take x y)) (number-sequence 1 (length fragments)) (-repeat (length fragments) fragments)))
-             (final-fragments (mapcar (lambda (x) (concat (s-join "" (cons projname (-interpose "/" x))) "/")) expanded-fragments)))
-
-            (vemv/safe-select-window vemv/project-explorer-window)
-              ;; (pe/fold-all) ;; necessary in principle, skip it for performance. seems to work fine.
-            (beginning-of-buffer)
-
-            (seq-doseq (f (butlast final-fragments))
-                       (while (not (string-equal f (pe/current-directory)))
-                         (next-line))
-                       (pe/return))
-
-            (while (not (string-equal (s-chop-suffix "/" (first (last final-fragments))) (pe/get-filename)))
-              (next-line))
-
-            (end-of-line)))))
+        (delay fallback 1)
+        
+        (vemv/refresh-file-caches)
+        (vemv/safe-select-window vemv/main_window)
+        (if (minibuffer-prompt)
+            (delay fallback 1)
+            
+            (let ((buffer-truename (file-truename (buffer-file-name))))
+              (when (vemv/contains? buffer-truename vemv/project-root-dir)
+                (let* ((buffer-fragments (-remove (lambda (x) (string-equal x "")) (split-string buffer-truename "/")))
+                       (projname (pe/project-root-function-default)) ;; "/Users/vemv/gpm"
+                       (project-fragments (-remove (lambda (x) (string-equal x "")) (split-string projname "/")))
+                       (fragments (-drop (length project-fragments) buffer-fragments))
+                       (expanded-fragments (mapcar* (lambda (x y) (-take x y)) (number-sequence 1 (length fragments)) (-repeat (length fragments) fragments)))
+                       (final-fragments (mapcar (lambda (x) (concat (s-join "" (cons projname (-interpose "/" x))) "/")) expanded-fragments)))
+                  
+                  (vemv/safe-select-window vemv/project-explorer-window)
+                  ;; (pe/fold-all) ;; necessary in principle, skip it for performance. seems to work fine.
+                  (beginning-of-buffer)
+                  
+                  (seq-doseq (f (butlast final-fragments))
+                    (while (not (string-equal f (pe/current-directory)))
+                      (next-line))
+                    (pe/return))
+                  
+                  (while (not (string-equal (s-chop-suffix "/" (first (last final-fragments))) (pe/get-filename)))
+                    (next-line))
+                  
+                  (end-of-line))))))))
 
 (defun vemv/safe-show-current-file-in-project-explorer* ()
   (condition-case nil
-                  (vemv/show-current-file-in-project-explorer)
-                  (error (ignore-errors (vemv/show-current-file-in-project-explorer))))
+                  (vemv/show-current-file-in-project-explorer-unsafe)
+                  (error (ignore-errors (vemv/show-current-file-in-project-explorer-unsafe))))
   (vemv/safe-select-window vemv/main_window))
 
 (setq vemv/safe-show-current-file-in-project-explorer
