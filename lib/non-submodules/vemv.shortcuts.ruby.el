3a5d979a8e068af96f8374676531047b0168d385
vemv <vemv@users.noreply.github.com>
2018-08-06 23:30:05 +0200

    vemv.shortcuts.ruby: bind some smartparens commands

--- lib/non-submodules/vemv.shortcuts.ruby.el
+++ lib/non-submodules/vemv.shortcuts.ruby.el
@@ -3,4 +3,8 @@
 (provide 'vemv.shortcuts.ruby)
 
 (setq vemv/shortcuts/ruby/secondary-up            'sp-splice-sexp
-      vemv/shortcuts/ruby/primary-backspace       'kill-line)
+      vemv/shortcuts/ruby/primary-backspace       'kill-line
+      vemv/shortcuts/ruby/secondary-k             (argless
+                                                   (call-interactively 'sp-backward-kill-sexp))
+      vemv/shortcuts/ruby/secondary-backspace     (argless
+                                                   (call-interactively 'sp-backward-kill-word)))
