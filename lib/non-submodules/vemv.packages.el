1bfd4dfc7ba9a0b3cda39537637388653d819f0c
vemv <vemv@users.noreply.github.com>
2018-08-09 23:55:11 +0200

    Byte-compile remaining libs

--- lib/non-submodules/vemv.packages.el
+++ lib/non-submodules/vemv.packages.el
@@ -9,6 +9,10 @@
 
 (add-to-list 'same-window-buffer-names "*Compile-Log*")
 
+(setq byte-compile-verbose nil)
+(setq byte-compile-warnings nil)
+(setq byte-compile-log-warning-function (argless))
+
 (setq vemv/packages-refreshed nil)
 
 (dolist (package '(edn inflections hydra company queue fiplr smartparens yasnippet multiple-cursors
@@ -19,16 +23,6 @@
        (package-refresh-contents)
        (setq vemv/packages-refreshed t))
      (package-install package))))
-
-;; Eases editing locally-modified packages.
-;; Example:
-;; Fork a package
-;; `ln -s` the relevant file to ~/.emacs.d/elpa/the-package
-;; rm ~/.emacs.d/elpa/the-package/foo.elc
-;; restart emacs.
-(add-hook 'compilation-finish-functions (lambda (b _)
-                                          (kill-buffer b)))
-(byte-recompile-directory (expand-file-name "~/.emacs.d/elpa") 0)
 
 (require 'saveplace)
 (require 'dash)
@@ -56,4 +50,24 @@
 (require 'helm-ag)
 (require 'desktop)
 (require 'smartparens-config)
+
+;; Eases editing locally-modified packages.
+;; Also makes things faster.
+;; Example:
+;; Fork a package, place the git repo outside ~/.emacs.d
+;; `ln -s` the relevant file to ~/.emacs.d/elpa/the-package/foo.el
+;; rm ~/.emacs.d/elpa/the-package/foo.elc
+;; restart emacs.
+
+(let* ((lib-dir (expand-file-name "~/.emacs.d/lib/"))
+       (dirs (->> lib-dir
+                  directory-files
+                  (-remove (lambda (x)
+                             (member x `("." ".." "non-submodules"))))
+                  (mapcar (lambda (x)
+                            (concat lib-dir x)))
+                  (cons (expand-file-name "~/.emacs.d/elpa")))))
+  (dolist (dir dirs)
+    (byte-recompile-directory dir 0)))
+
 (provide 'vemv.packages)
