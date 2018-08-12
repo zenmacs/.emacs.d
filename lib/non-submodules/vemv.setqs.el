baedfa4dd87d710960a0b4058695dbc4d36ba737
vemv <vemv@users.noreply.github.com>
2018-08-13 00:09:39 +0200

    Expand `fiplr-ignored-globs`

--- lib/non-submodules/vemv.setqs.el
+++ lib/non-submodules/vemv.setqs.el
@@ -164,7 +164,7 @@ of the buffer into a formatted string."
 (setq fiplr-ignored-globs
       ;; `directories` entries must be single-segment, i.e `/` doesn't work.
       '((directories (".git" "tmp" ".svn" ".hg" ".bzr" "tools" "res-vagrant" "resources" ".lumo-cache"
-                      ".paket" "doc" "bin" "assets" "public" "node_modules" "coverage"))
+                      ".paket" "doc" "bin" "assets" "public" "node_modules" "coverage" "target"))
         (files (".#*" "*~" "*.so" "*.jpg" "*.png" "*.gif" "*.pdf" "*.gz" "*.zip" "*.DS_Store"
                 "*.md" "*.gitgnore" "*.scssc" "*.keep" "*.json" "LICENSE" "LICENCE" "license" "*.patch"
                 "flask-server" "Makefile" "makefile" "*.txt" "*ignore""*.*rc" "*.map" ".last-compilation-digest-development"
