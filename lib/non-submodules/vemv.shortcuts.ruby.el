22452f9b71c77f42471f09e69a6d0a2a153e543f
vemv <vemv@users.noreply.github.com>
2018-08-13 05:01:26 +0200

    Expand `vemv.shortcuts.ruby`

--- lib/non-submodules/vemv.shortcuts.ruby.el
+++ lib/non-submodules/vemv.shortcuts.ruby.el
@@ -2,9 +2,16 @@
 (require 'vemv.shortcuts.ruby.base)
 (provide 'vemv.shortcuts.ruby)
 
-(setq vemv/shortcuts/ruby/secondary-up            'sp-splice-sexp
-      vemv/shortcuts/ruby/primary-backspace       'kill-line
-      vemv/shortcuts/ruby/secondary-k             (argless
-                                                   (call-interactively 'sp-backward-kill-sexp))
-      vemv/shortcuts/ruby/secondary-backspace     (argless
-                                                   (call-interactively 'sp-backward-kill-word)))
+(setq vemv/shortcuts/ruby/secondary-up               (argless
+                                                      (call-interactively 'sp-splice-sexp))
+      vemv/shortcuts/ruby/primary-backspace          'kill-line
+      vemv/shortcuts/ruby/secondary-k                (argless
+                                                      (call-interactively 'sp-backward-kill-sexp))
+      vemv/shortcuts/ruby/secondary-backspace        (argless
+                                                      (call-interactively 'sp-backward-kill-word))
+      vemv/shortcuts/ruby/secondary-left-bracket     (argless
+                                                      (call-interactively 'sp-backward-sexp))
+      vemv/shortcuts/ruby/secondary-left-parens      (argless
+                                                      (call-interactively 'sp-wrap-round))
+      vemv/shortcuts/ruby/secondary-right-bracket    (argless
+                                                      (call-interactively 'sp-forward-sexp)))
