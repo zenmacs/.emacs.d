7603212cbf23a98a5e084014d1446e40e5c867d1
vemv <vemv@users.noreply.github.com>
2018-08-29 14:12:16 +0200

    Don't use ruby-mode for Gemfile.lock

--- lib/non-submodules/vemv.packages.el
+++ lib/non-submodules/vemv.packages.el
@@ -95,7 +95,7 @@
              '("\\.\\(?:cap\\|gemspec\\|irbrc\\|gemrc\\|rake\\|rb\\|ru\\|thor\\)\\'" . ruby-mode))
 
 (add-to-list 'auto-mode-alist
-             '("\\(?:Brewfile\\|Capfile\\|Gemfile\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'" . ruby-mode))
+             '("\\(?:Brewfile\\|Capfile\\|Gemfile$\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'" . ruby-mode))
 
 ;; Eases editing locally-modified packages.
 ;; Also makes things faster.
