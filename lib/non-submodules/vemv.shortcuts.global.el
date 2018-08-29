ca72189aaf360a457e65f70fcca305d29714f778
vemv <vemv@users.noreply.github.com>
2018-08-29 08:33:47 +0200

    Make S-RET able to clear `*messages*`

--- lib/non-submodules/vemv.shortcuts.global.el
+++ lib/non-submodules/vemv.shortcuts.global.el
@@ -137,12 +137,15 @@
 (global-set-key [(shift return)] (argless
                                   (if (vemv/in-a-clojure-mode?)
                                       (vemv/clear-cider-repl-buffer)
-                                    (vemv/save-window-excursion
-                                     (vemv/safe-select-window vemv/repl-window)
-                                     (if (vemv/in-a-lisp-mode?)
-                                         (switch-to-buffer "*ielm*")
-                                       (switch-to-buffer "*shell-1*"))
-                                     (comint-clear-buffer)))))
+                                    (if (eq major-mode 'messages-buffer-mode)
+                                        (let ((inhibit-read-only t))
+                                          (erase-buffer))
+                                      (vemv/save-window-excursion
+                                       (vemv/safe-select-window vemv/repl-window)
+                                       (if (vemv/in-a-lisp-mode?)
+                                           (switch-to-buffer "*ielm*")
+                                         (switch-to-buffer "*shell-1*"))
+                                       (comint-clear-buffer))))))
 
 ;; same here. control-ret is interpreted as s-return rather than as tertiary-RET
 (global-set-key [(s return)] 'vemv/load-clojure-buffer)
