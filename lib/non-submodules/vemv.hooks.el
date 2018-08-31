cb2acb4e1e030acfb2c0436409c59616f4c19d64
vemv <vemv@users.noreply.github.com>
2018-08-31 08:48:54 +0200

    Expand `paren-face-regexp` for elisp

--- lib/non-submodules/vemv.hooks.el
+++ lib/non-submodules/vemv.hooks.el
@@ -258,7 +258,7 @@
 
 (add-hook 'emacs-lisp-mode-hook (argless
                                  (enable-paredit-mode)
-                                 (setq-local paren-face-regexp "[][()/#']")
+                                 (setq-local paren-face-regexp "[][()/#'`,]")
                                  (paren-face-mode 1)))
 
 (add-hook 'ielm-mode-hook 'enable-paredit-mode)
