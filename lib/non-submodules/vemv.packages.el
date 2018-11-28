281b9cfea4c3ac693a1a0dd2fb6e9bcd4bee4165
vemv <vemv@users.noreply.github.com>
2018-11-28 21:00:29 +0100

    MELPA: use https

--- lib/non-submodules/vemv.packages.el
+++ lib/non-submodules/vemv.packages.el
@@ -3,7 +3,7 @@
 (require 'package)
 
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
 (package-initialize)
 
