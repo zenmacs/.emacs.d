0b205ca8d818381b08995de9be12c77d7984ff19
vemv <vemv@users.noreply.github.com>
2020-02-18 00:37:56 +0100

    clojure-mode: draft `s-;` support

--- lib/non-submodules/vemv.hooks.el
+++ lib/non-submodules/vemv.hooks.el
@@ -444,6 +444,8 @@
                                 (vemv/set-keys-for-scope clojure-mode-map vemv/clojure-key-bindings))
                               (define-key clojure-mode-map (kbd "C-M-0") 'hydra-cljr-help-menu/body)
                               (define-key clojure-mode-map (kbd ";") 'vemv/semicolon)
+                              (define-key clojure-mode-map (kbd "s-;") (argless
+                                                                        (insert "#_ ")))
                               (define-key clojure-mode-map (kbd "<tab>") 'vemv/tab)
                               (define-key clojure-mode-map (kbd "<s-tab>") 'vemv.message-clojure-doc/of-current-invocation)
                               ;; XXX backtab not handled by gen.rb
