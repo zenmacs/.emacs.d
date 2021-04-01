e8a08f8e595c77929414fb97882d510fa6e566df
vemv <vemv@users.noreply.github.com>
2021-04-01 04:03:36 +0200

    `cider-connected-hook`: remove `delay`
    This `delay` didn't have a clear purpose (I couldn't determine it via git; dates back to 2017) and was a nuisance.
    
    ...most likely the reason for the delay was that the cljs repl tended to show up (at the time) 2s after the clj one.
    
    (the problem might resurface if I worked on cljs projects again, but then again probably I'd change fighweel -> shadow-cljs, etc)

--- lib/non-submodules/vemv.hooks.el
+++ lib/non-submodules/vemv.hooks.el
@@ -460,14 +460,12 @@
 
 (add-hook 'cider-connected-hook
           (argless
-           (delay (argless
-                   (setq vemv-cider-connecting nil)
-                   (setq vemv-cider-connected t)
-                   (vemv/show-clj-or-cljs-repl)
-                   (when (not vemv-cleaning-namespaces)
-                     (vemv/advice-nrepl))
-                   (define-key clojure-mode-map (kbd "/") 'cljr-slash))
-                  2)))
+           (setq vemv-cider-connecting nil)
+           (setq vemv-cider-connected t)
+           (vemv/show-clj-or-cljs-repl)
+           (when (not vemv-cleaning-namespaces)
+             (vemv/advice-nrepl))
+           (define-key clojure-mode-map (kbd "/") 'cljr-slash)))
 
 (add-hook 'cider-repl-mode-hook #'paredit-mode)
 
